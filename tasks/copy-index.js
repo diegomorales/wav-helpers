7172f652d9556e4c487375596fca6fb05b4fffe9
Diego Morales <diego.morales@gmx.net>
2020-05-24 21:59:52 +0200

    clean up

--- tasks/copy-index.js
+++ tasks/copy-index.js
@@ -12,7 +12,7 @@ module.exports = function copyIndex (done) {
     for (const i in manifest) {
       if (Object.prototype.hasOwnProperty.call(manifest, i)) {
         if (i.includes('.css')) {
-          // Inline CSS
+          // Get CSS
           inlineStyle = fs.readFileSync(paths.build + i).toString()
 
           // Remove sourcemap reference.
@@ -31,30 +31,4 @@ module.exports = function copyIndex (done) {
   fs.writeFileSync(paths.build + 'index.html', rendered, 'utf8')
 
   done()
-
-  // return gulp.src(paths.dev + 'index.html')
-  //   .pipe(gulp.dest(function (file) {
-  //     // let fileContent = file.contents.toString()
-  //     //
-  //     // for (const i in manifest) {
-  //     //   if (Object.prototype.hasOwnProperty.call(manifest, i)) {
-  //     //     if (isProd && i.includes('.css')) {
-  //     //       // Inline CSS
-  //     //       const regex = new RegExp(`<link.*${i}.*>`)
-  //     //       let css = fs.readFileSync(paths.build + i).toString()
-  //     //
-  //     //       // Remove sourcemap reference.
-  //     //       css = css.replace(/\/\*.*\*\//, '')
-  //     //
-  //     //       fileContent = fileContent.replace(regex, `<style>${css}</style>`)
-  //     //     } else {
-  //     //       fileContent = fileContent.replace(i, manifest[i])
-  //     //     }
-  //     //   }
-  //     // }
-  //     //
-  //     // file.contents = Buffer.from(fileContent)
-  //
-  //     return paths.build
-  //   }))
 }
